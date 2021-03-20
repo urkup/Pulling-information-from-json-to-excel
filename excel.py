53f162061b92fcba8457e6a95490ede78b0c483b
IBRAHIM HALIL URKUP <80271155+urkup@users.noreply.github.com>
2021-03-20 23:57:40 +0300

    Add files via upload

---
+++ excel.py
@@ -0,0 +1,12 @@
+import json
+import csv
+with open("./users10.json") as file:
+    data = json.load(file)
+
+fname = "output3.csv"
+
+with open(fname, "w") as file:
+    csv_file = csv.writer(file)
+    csv_file.writerow(["first_name","custom_email","username"])
+    for item in data["results"]:
+        csv_file.writerow([item["first_name"],item["custom_email"],item["username"]])
